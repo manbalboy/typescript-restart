4ab34a3708b91cbe77eaa3e3f8dbf8a390480955
manbalboy <manbalboy@hanmail.net>
2021-04-05 22:43:49 +0900

    타입스크립트 또다른 시작

---
+++ src/1 copy.ts
@@ -0,0 +1,5 @@
+
+const v1 = 1233;
+const v2 = () => console.log('1234312321');
+
+console.log(v1);
---
+++ src/1.ts
@@ -0,0 +1,7 @@
+import { isEqual } from "lodash";
+
+const v1 = 1233;
+const v2 = () => console.log('1234312321');
+
+
+// isEqual()
