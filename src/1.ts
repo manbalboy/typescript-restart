864c07a64e41258d0719e686cd82d5d77460817f
manbalboy <manbalboy@hanmail.net>
2021-04-06 22:39:55 +0900

    타입스크립트

--- src/1.ts
+++ src/1.ts
@@ -1,7 +1,20 @@
-import { isEqual } from "lodash";
+export {};
 
-const v1 = 1233;
-const v2 = () => console.log('1234312321');
+// const v2 = () => console.log('1234312321');
 
+
+const size: number = 123;
+const isBig: boolean = size >= 100;
+const msg: string = isBig ? '크다' : '작다';
+
+// const v1: undefined = undefined;
+// const v2: null = null;
+function test (): void {
+
+}
+
+
+
+console.log(123);
 
 // isEqual()
