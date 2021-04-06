205be6c12b7261cd0e72c64dfe3fbe2c97b28251
manbalboy <manbalboy@hanmail.net>
2021-04-06 23:09:57 +0900

    typescript 학습

--- src/1.js
+++ src/1.js
@@ -1,3 +1,12 @@
 "use strict";
-var v1 = 123;
-var v2 = function () { return console.log('123'); };
+exports.__esModule = true;
+// const v2 = () => console.log('1234312321');
+var size = 123;
+var isBig = size >= 100;
+var msg = isBig ? '크다' : '작다';
+// const v1: undefined = undefined;
+// const v2: null = null;
+function test() {
+}
+console.log(123);
+// isEqual()
